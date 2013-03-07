678d69df7b36490a5dc85f5fceec2b3aa8abd862
Markus Klinik <markus.klinik@gmx.de>
2013-03-07 14:14:41 +0100

    comments, code formatting, cleanup

--- comparePolynomial.m
+++ comparePolynomial.m
@@ -1,30 +1,70 @@
+// File comparePolynomial.m
+
+// Compares the runtime behaviour of several polynomial multiplication
+// algorithms.
+
+// Use as follows:
+// - Run it with the following command, then grab a cup of coffee and wait.
+//    $ magma -b comparePolynomial.m | tee out.txt
+// - Amend out.txt to remove the first few lines where it says "loading
+// polynomial.m".
+// - Use gnuplot to plot the result
+//    $ gnuplot plot.txt
+//   where plot.txt is a file containing these lines:
+//    set terminal png
+//    set output "polynomial_measurements.png"
+//    set grid
+//    plot [:] [-1:] for [IDX=0:3] 'out.txt' i IDX u 1:2 w lines title columnheader(1)
+
 load "polynomial.m";
 
+MAX_DEGREE := 3000
+
+// Runs the given polynomial multiplication algorithm with polynomials of
+// increasing degree, and output the running time on stdout.
 procedure run(f, name, coerce)
   print "\n\n";
   print "\"", name, "\"";
-  for i := 2 to 3000 by 55 do
+  for i := 2 to MAX_DEGREE by 55 do
     t1 := Cputime();
-    /*for j := 0 to 1 do*/
       _ := f(coerce([1..i]), coerce([1..i]));
-      /*_ := coerce([1..i]);*/
-    /*end for;*/
     t2 := Cputime(t1);
     print i, t2;
   end for;
 end procedure;
 
+
+// Wrap the built in multiplication operator in a function, so we can pass it to
+// `run`.
 function multiplyBuiltin(f, g)
   return f * g;
 end function;
 
-F := FiniteField(12289);
+// This magic number is the smallest prime p such that p - 1 mod 2^k = 1 where k
+// is the smallest number such that d < 2^k where d is the degree of the
+// polynomials we want to multiply.  If you increase the value MAX_DEGREE, adapt
+// p accordingly.
+p := 12289
+
+// use this snippet to calculate an appropriate prime number to use as the field
+// size
+// p := 1;
+// for i := 1 to 100000 do
+//   p := NextPrime(p);
+//   if p mod 4096 eq 1
+//     then print p;
+//   end if;
+// end for;
+
+F := FiniteField(p);
 P := PolynomialRing(F);
+
+// Coerce every element in a sequence to be of the field F.
 function coerce(s)
   return [F!x : x in s];
 end function;
 
-run(multiplyBuiltin, "builtin", func<s|P!s>);
+run(multiplyBuiltin, "builtin", func<s|P!s>); // coerces s to be a P-polynomial
 run(multiplyClassically, "classically", coerce);
 run(multiplyKaratsuba, "Karatsuba", coerce);
 run(multiplyDFT, "DFT", coerce);
--- polynomial.m
+++ polynomial.m
@@ -1,12 +1,18 @@
-// Sanitizes accessing sequences.
-// 1) sequence indexes start with 0
-// 2) non-existing elements are filled with 0
+// File polynomial.m
+//
+// Several variations of polynomial multiplication.
+// Polynomials are represented as sequences of their coefficients.
+
+// Helper for accessing elements of sequences.
+// - sequence indexes start with 0
+// - non-existing elements are filled with 0
 function get(s, i)
   if #s gt i
     then return s[i+1];
     else return 0;
   end if;
 end function;
+
 
 // Splits a sequence into two.
 // The first result sequence has n elements, while the second one has #s - n
@@ -16,8 +22,10 @@ function split(s, n)
     [s[i] : i in [n+1 .. #s]] ;
 end function;
 
+
 // Multiplies a polynomail by x^n.
-// This effectively shifts all coefficients n places to the right.
+// For our representation of polynomials as sequences, this means shifting all
+// coefficients n places to the right.
 function shift(s, n)
   result := s;
   for i := 1 to n do
@@ -26,7 +34,9 @@ function shift(s, n)
   return result;
 end function;
 
-multiplyClassically := function(a, b)
+
+// Multiplies two polynomials using the convolution product.
+function multiplyClassically(a, b)
    result := [];
    for x := 0 to (#a - 1) + (#b - 1) do
      sum := 0;
@@ -38,21 +48,26 @@ multiplyClassically := function(a, b)
    return result;
 end function;
 
+
 // Adds two sequences point-wise
 function addPointwise(s, t)
   return [get(s, i-1) + get(t, i-1) : i in [1..Max(#s, #t)]];
 end function;
+
 
 // Subtracts two sequences point-wise
 function subtractPointwise(s, t)
   return [get(s, i-1) - get(t, i-1) : i in [1..Max(#s, #t)]];
 end function;
 
+
 // Multiplies two sequences point-wise
 function multiplyPointwise(s, t)
   return [get(s, i-1) * get(t, i-1) : i in [1..Max(#s, #t)]];
 end function;
 
+
+// Multiplies two polynomials using Karatsuba's method
 function multiplyKaratsuba (F, G)
   if #F lt 2 and #G lt 2
     then
@@ -74,13 +89,13 @@ end function;
 
 
 // Zips two sequences.
-// ( s0, t0, s1, t1, s2, t2, ... )
-// s and t must be of the same length
+// Given two sequences s and t, the result will be
+//   [ s0, t0, s1, t1, s2, t2, ... ]
+// s and t must be of the same length.
 function zip(s, t)
   if #s eq 0 then return [];
   else
   result := [s[1], t[1]];
-  /*result := [];*/
   for i := 1 to #s do
     Append(~result, s[i]);
     Append(~result, t[i]);
@@ -91,6 +106,12 @@ end function;
 
 
 // Discrete Fourier Transform
+// Inputs:
+//   k: An integer such that 2^k is larger than the degree of f.
+//   f: A polynomial with coefficients in a finite field where appropriate
+//      primitive roots exist.  The degree of the polynomial must be smaller
+//      than 2^k.
+//   w: Any primitive 2^k-th root of unity
 function DFT(k, f, w)
   if k eq 0
     then
@@ -106,12 +127,14 @@ function DFT(k, f, w)
 end function;
 
 
-// The inverse DFT
+// Inverse DFT
+// For a description of the parameters, see the DFT function.
 function DFTinv(k, f, w)
   return [(1 / (2^k)) * a : a in DFT(k, f, w^(-1))];
 end function;
 
-// Actual implementation of the polynomial multiplication.
+
+// Actual implementation of the polynomial multiplication by DFT.
 // Don't use this function directly, use multiplyDFT instead.
 function multiplyDFT_(f, g, w, k)
   a := DFT(k, f, w);
@@ -120,24 +143,15 @@ function multiplyDFT_(f, g, w, k)
   return DFTinv(k, c, w);
 end function;
 
+
 // Multiplies two polynomials using the DFT method.
 // Both f and g must be sequences of elements of a finite field.
-// f must be non-empty
+// f must be non-empty.
 function multiplyDFT(f, g)
   F := Parent(f[1]);
   k := 1 + Floor(Log(2, Max(#f - 1, #g - 1)));
   w := RootOfUnity(2^k, F);
   return multiplyDFT_(f, g, w, k);
 end function;
-
-procedure foobar()
-  F := FiniteField(13);
-  /*f := [F!2, F!3, F!4, F!2];*/
-  f := [F!3, F!4, F!2];
-  g := DFT(2, f, 8);
-  g;
-  h := DFTinv(2, g, 8);
-  h;
-end procedure;
 
 // vim: ft=magma expandtab ts=2 sw=2 nocindent
