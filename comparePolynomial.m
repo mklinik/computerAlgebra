4ab053bf66576f139ad1277bea7f69cb7c8fd8b2
Markus Klinik <markus.klinik@gmx.de>
2013-03-07 11:39:01 +0100

    compare polynomials by coercing to finite field 13

--- comparePolynomial.m
+++ comparePolynomial.m
@@ -1,20 +1,33 @@
 load "polynomial.m";
 
-procedure run(f, name)
+procedure run(f, name, coerce)
   print "\n\n";
   print "\"", name, "\"";
-  for i := 0 to 2000 by 50 do
+  for i := 2 to 2000 by 51 do
     t1 := Cputime();
     /*for j := 0 to 1 do*/
-      _ := f([1..i], [1..i]);
+      _ := f(coerce([1..i]), coerce([1..i]));
+      /*_ := coerce([1..i]);*/
     /*end for;*/
     t2 := Cputime(t1);
     print i, t2;
   end for;
 end procedure;
 
-run(multiplyClassically, "classically");
-run(multiplyKaratsuba, "Karatsuba");
+function multiplyBuiltin(f, g)
+  return f * g;
+end function;
+
+F := FiniteField(13);
+P := PolynomialRing(F);
+function coerce(s)
+  return [F!x : x in s];
+end function;
+
+run(multiplyBuiltin, "builtin", func<s|P!s>);
+run(multiplyClassically, "classically", coerce);
+run(multiplyKaratsuba, "Karatsuba", coerce);
+run(multiplyDFT, "DFT", coerce);
 
 quit;
 // vim: ft=magma expandtab ts=2 sw=2
