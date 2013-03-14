b90213e56bf693c4084e68f472ec60de8815963a
Markus Klinik <markus.klinik@gmx.de>
2013-03-14 11:12:02 +0100

    division free gaussian elimination

--- gauss.m
+++ gauss.m
@@ -8,23 +8,46 @@ function firstNonZeroRow(col, M)
   return 0; // none found
 end function;
 
-procedure gauss(~M)
+procedure gaussSimple(~M)
   // for each max(columns, rows):
-  for cur := 1 to Min(NumberOfRows(M), NumberOfColumns(M)) do
-    // find a row j where the first element is not zero
-    j := firstNonZeroRow(cur, M);
-    if j ne 0 then
-      // swap j with the current row
-      SwapRows(~M, cur, j);
+  for col := 1 to Min(NumberOfRows(M), NumberOfColumns(M)) do
+    // find a row fnz where the first element is not zero
+    fnz := firstNonZeroRow(col, M);
+    if fnz ne 0 then
+      // swap fnz with the current row
+      SwapRows(~M, col, fnz);
       // for each row below the current row:
-      for cur2 := cur + 1 to NumberOfRows(M) do
+      for cur2 := col + 1 to NumberOfRows(M) do
         // if 1st element is non-zero,
         //   multiply with inverse of 1st elt
-        //   and subtract multiple of cur from cur2
-        curElt := M[cur2, cur];
+        //   and subtract multiple of col from cur2
+        curElt := M[cur2, col];
         if curElt ne 0 then
           MultiplyRow(~M, 1/curElt, cur2);
-          AddRow(~M, -1/M[cur,cur], cur, cur2);
+          AddRow(~M, -1/M[col,col], col, cur2);
+        end if;
+      end for;
+    end if;
+  end for;
+end procedure;
+
+procedure gaussDivisionFree(~M)
+  // for each max(columns, rows):
+  for col := 1 to Min(NumberOfRows(M), NumberOfColumns(M)) do
+    // find a row fnz where the first element is not zero
+    fnz := firstNonZeroRow(col, M);
+    if fnz ne 0 then
+      // swap fnz with the current row
+      SwapRows(~M, col, fnz);
+      // for each row below the current row:
+      for cur2 := col + 1 to NumberOfRows(M) do
+        // if 1st element is non-zero,
+        curElt := M[cur2, col];
+        if curElt ne 0 then
+          for j := col to NumberOfColumns(M) do
+            M[cur2,j] :=
+              M[col,col] * M[cur2,j] - M[col,j] * curElt;
+          end for;
         end if;
       end for;
     end if;
@@ -33,7 +56,7 @@ end procedure;
 
 procedure test()
   SetSeed(0);
-  m := RandomMatrix(FiniteField(13), 5, 5);
+  m := RandomMatrix(FiniteField(13), 5, 15);
   /*m := Matrix(RationalField(), 6, 4,*/
   /*[[1, 1, 2, 3]*/
   /*,[2, 5, 6, 7]*/
@@ -45,7 +68,8 @@ procedure test()
   print m;
   print "-----";
   /*print firstNonZeroRow(1, m);*/
-  gauss(~m);
+  /*gaussDivisionFree(~m);*/
+  gaussSimple(~m);
   print m;
 end procedure;
 
