23f888b4074ffaa8ef1d6b82b3cd888c6e05cb7e
Markus Klinik <markus.klinik@gmx.de>
2013-04-11 16:11:26 +0200

    Reduce

--- groebner.m
+++ groebner.m
@@ -17,6 +17,7 @@
 N := 2;
 zeroPolynomial := [];
 zeroMonomial := [ 0 : x in [1..N] ];
+zeroTerm := <0, zeroMonomial>;
 
 /*
  * Left fold
@@ -45,13 +46,40 @@ function foldl1(f, xs)
 end function;
 
 /*
+ * Returns all elements of the sequence that satisfy the predicate
+ */
+function filter(p, xs)
+  result := [];
+  for i:=1 to #xs do
+    if p(xs[i]) then Append(~result, xs[i]);
+    end if;
+  end for;
+  return result;
+end function;
+
+function zipWith(f, xs, ys)
+  acc := [];
+  for i:=1 to #xs do
+    Append(~acc, f(xs[i], ys[i]));
+  end for;
+  return acc;
+end function;
+
+function all(p, xs)
+  return foldl(func< b, x | b and p(x)>, true, xs);
+end function;
+
+function id(x)
+  return x;
+end function;
+
+/*
  * We define the ordering on monomials as the lexicographical ordering, so
- * comparing two monomials amounts to comparing the tuples representing them.
+ * comparing two monomials amounts to comparing the sequences representing them.
  */
 function monomialGt(m1, m2)
   return m1 gt m2;
 end function;
-
 
 /*
  * Given two terms, returns the term with the greater monomial
@@ -60,16 +88,11 @@ function maxTerm(t1, t2)
   return monomialGt(t1[2], t2[2]) select t1 else t2;
 end function;
 
-/*
- * Returns the leading term of the polynomial f
- */
 function leadingTerm(f)
-  return #f eq 0 select zeroPolynomial else foldl1(maxTerm, f);
+  // if f is the zero polynomial, we return the minus infinity mononomial
+  return foldl(maxTerm, <0, []>, f);
 end function;
 
-/*
- * Returns the leading monomial of the polynomial f
- */
 function leadingMonomial(f)
   return leadingTerm(f)[2];
 end function;
@@ -78,13 +101,113 @@ function leadingCoefficient(f)
   return leadingTerm(f)[1];
 end function;
 
+function greaterThanEqual(x, y)
+  return x ge y;
+end function;
+
+/*
+ * Monomial m1 divides m2 if every exponent of m2 is greater or equal to the corresponding exponent
+ * of m1.
+ */
+function divides(m1, m2)
+  return all(id, zipWith(greaterThanEqual, m2, m1));
+end function;
+
+function minus(x, y)
+  return x - y;
+end function;
+
+function plus(x, y)
+  return x + y;
+end function;
+
+/*
+ * Divides two monomials by subtracting the representing lists elementwise
+ */
+function divideMonomial(m1, m2)
+  return zipWith(minus, m1, m2);
+end function;
+
+function multiplyMonomial(m1, m2)
+  return zipWith(plus, m1, m2);
+end function;
+
+function divideTerm(t1, t2)
+  return <t1[1] / t2[1], divideMonomial(t1[2], t2[2])>;
+end function;
+
+function multiplyTerm(t1, t2)
+  return <t1[1] * t2[1], multiplyMonomial(t1[2], t2[2])>;
+end function;
+
+/*
+ * Multiplies a term t to a polynomial f
+ */
+function multiplyTermPolynomial(t, f)
+  if t[1] eq 0 then return zeroPolynomial;
+  end if;
+
+  result := [];
+  for i:=1 to #f do
+    Append(~result, multiplyTerm(t, f[i]));
+  end for;
+  return result;
+end function;
+
+function subtractPolynomial(f1, f2)
+  g := f1;
+  for j:=1 to #f2 do
+    processed := false;
+    for i:=1 to #f1 do
+      if f1[i][2] eq f2[j][2] // the monomials are the same
+        then
+          g[i][1] -:= f2[j][1]; // subtract the coefficients
+          processed := true;
+      end if;
+    end for;
+    if not processed
+      then Append(~g, <-f2[j][1], f2[j][2]>);
+    end if;
+  end for;
+  return filter(func<t | t[1] ne 0>, g); // remove all zero terms
+end function;
+
+function reduce(B, f)
+  J := [ b : b in B | divides(leadingMonomial(b), leadingMonomial(f)) ];
+  if #J ne 0
+    then
+      b := J[1];
+      ltf := leadingTerm(f);
+      ltb := leadingTerm(b);
+      return reduce(B, subtractPolynomial(f, multiplyTermPolynomial(divideTerm(ltf, ltb), b)));
+    else
+      return f;
+  end if;
+end function;
+
 procedure main()
-  f := [<1000, [5, 1]>, <2, [1, 2]>];
-  print(f);
-  lm := leadingMonomial(f);
-  print(lm);
-  lc := leadingCoefficient(f);
-  print(lc);
+  // f := [<1000, [5, 1]>, <2, [1, 2]>];
+  // print(f);
+  // lm := leadingMonomial(f);
+  // print(lm);
+  // lc := leadingCoefficient(f);
+  // print(lc);
+
+
+  // example 3.1
+  f := [ <1, [2,2]> ];
+  g := [<-1, [0,0]>, <1, [2,1]>];
+  h := [<-1, [0,0]>, <1, [1,2]>];
+  B := [g, h];
+
+  /*print(leadingMonomial(f));*/
+
+  /*print(subtractPolynomial(g, h));*/
+  /*print(subtractPolynomial(f, g));*/
+
+  print(reduce(B, f));
 end procedure;
+
+// main();
 
 // vim: ft=magma
