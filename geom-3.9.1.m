7fbd644ebe1c246756db757031d9dcfeabcb6a47
Markus Klinik <markus.klinik@gmx.de>
2013-06-12 14:21:08 +0200

    fix hypothesis

--- geom-3.9.1.m
+++ geom-3.9.1.m
@@ -1,8 +1,8 @@
 
-S<a,b1,b2,z> := PolynomialRing(RationalField(), 5);
+S<a,b1,b2,z> := PolynomialRing(AlgebraicClosure(RationalField()), 4);
 
-h1 := b1^2 * (a - b1)^2 - b2^4;
-t := a * b1 - b1^2 - b2^2;
+h1 := b1 * (a - b1) - b2^2;
+t := a^2 * b1^2 - (b1^2 + b2^2)^2;
 
 I := ideal<S | h1, t * z - 1>;
 
