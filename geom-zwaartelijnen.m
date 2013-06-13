6a87efcd02664a40d49f60a0e64a2b413d3eeb29
Markus Klinik <markus.klinik@gmx.de>
2013-06-13 15:09:23 +0200

    triangle stuff

--- geom-zwaartelijnen.m
+++ geom-zwaartelijnen.m
@@ -1,11 +1,12 @@
-// In an arbitrary triangle, all altitudes meet at the same point
+// In an arbitrary triangle, all medians meet at the same point
 
 S<e1,e2,f1,f2,a1,a2,m1,m2,z> := PolynomialRing(AlgebraicClosure(RationalField()), 9);
 
-// FM _|_ AB
-h1 := (m1-f1)*(2-a1) + (m2-f2)*(-a2);
-// EM _|_ AC
-h2 := (m1-e1)*(-a1) + (m2-e2)*(-a2);
+// M lies on CF
+h1 := f1*m2 - f2*m1;
+
+// M lies on BE
+h2 := m2*(e1-2) - e2*(m1-2);
 
 // E in the middle of AC
 h3 := 2*e1-a1;
@@ -15,8 +16,8 @@ h4 := 2*e2-a2;
 h5 := 2*f1-2-a1;
 h6 := 2*f2-a2;
 
-// DM _|_ BC
-t  := m1 - 1;
+// M lies on DA
+t  := (1-a1)*(-m2)-(-a2)*(1-m1);
 
 I := ideal<S | h1, h2, h3, h4, h5, h6, t * z - 1>;
 NormalForm(S!1, I);
