c981315eb8e6d24a9e2ddffcb6ac5c319e922e5d
Markus Klinik <markus.klinik@gmx.de>
2013-06-20 11:19:49 +0200

    more tests

--- poly_fac_test.m
+++ poly_fac_test.m
@@ -2,4 +2,8 @@ load "polynomial_factorization.m";
 
 // Example page 357
 f := x * (x+1) * (x^2 + 1) * (x^2 + x + 2);
-distinctDegreeFactorization(f);
+distinctDegreeFactorization(f) eq [x^2 + x, x^4 + x^3 + x + 2];
+
+// Example 14.3
+f := x^8 + x^7 - x^6 + x^5 - x^3 - x^2 - x;
+distinctDegreeFactorization(f) eq [x, x^4 + x^3 + x - 1, x^3 - x + 1];
